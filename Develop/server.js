18a5f1ba613a67b7b492ffd98f44428e5507d688
Claire Bohlen <clairebohlen@gmail.com>
2020-11-16 18:47:07 -0700

    adding server js file and setting up connection

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require("express"); //requring express 
+const logger = require("morgan"); // requireing middleware
+const mongoose = require("mongoose"); //requiring mongoose package
+
+const PORT = process.env.PORT || 3000; 
+
+const db = require("./models"); //requiring mongoose schemas
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
