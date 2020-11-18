0a98fca93b978ce759f59244dd66562396eb7c89
Claire Bohlen <clairebohlen@gmail.com>
2020-11-17 18:46:10 -0700

    adding working schema data

--- Develop/models/workouts.js
+++ Develop/models/workouts.js
@@ -1,6 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const WorkoutSchema = new Schema ({
+
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+            {
+
+                type:{
+                    type: String,
+                    required: true,
+
+                },
+                weight: {
+                    type: Number,
+                    required: true,
+
+                },
+                reps: {
+                    type: Number,
+                    required: true,
+
+                },
+                sets: {
+                    type: Number,
+                    required: true,
+
+                },
+                duration:{
+                    type: Number,
+                    required: true,
+
+                },
+                distance:{
+                    type: Number,
+                    required: true,
+
+                }
+            }
+    ]
+})
 
 
-module.exports = Note;
+
+const Workout = mongoose.model("Workout", WorkoutSchema )
+module.exports = Workout;
